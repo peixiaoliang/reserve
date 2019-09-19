86dd2c33d8a7f07ea799cc18b366f9f1e51d7e37
peixiaoliang <1561201507@qq.com>
2019-09-19 11:22:53 +0800

    919

---
+++ js/index.js
@@ -0,0 +1,78 @@
+$(function(){
+    $(".resgiterBtn").click(function(){
+        $(".container2").show();
+    })
+    $(".agreeBtn").click(function(){
+        $(".container1").hide();
+        $(".container2").hide();
+    })
+    $(".submit").click(function(){
+        // console.log($("form").serialize());
+        // console.log($("form").serialize());
+        var formData=$("form").serialize();
+        if($("form input[name='name']").val().length<2){
+            alert("请输入正确的名字");
+            return false;
+        }
+        if($("form input[name='phone']").val().length<11){
+            alert("请输入正确的电话号码");
+            return false;
+        }
+        if($("form input[name='company']").val().length<2){
+            alert("请输入正确的公司名称");
+            return false;
+        }
+        if($("form input[name='brand']").val().length<2){
+            alert("请输入正确的品牌名称");
+            return false;
+        }
+        if($("form input[name='job']").val().length<2){
+            alert("请输入正确的职务");
+            return false;
+        }
+        $.ajax({
+            url: './php/index.php?'+formData,
+            async: false,
+            type: 'post',
+            success: function(data){
+                console.log(data)
+            }
+        })
+    })
+    function generateQRCode(rendermethod, picwidth, picheight, url) {  
+        $("#qrcode").qrcode({   
+            render: rendermethod, // 渲染方式有table方式（IE兼容）和canvas方式  
+            width: picwidth, //宽度   
+            height:picheight, //高度   
+            text: utf16to8(url), //内容   
+            typeNumber:-1,//计算模式  
+            correctLevel:2,//二维码纠错级别  
+            background:"transparent",//背景颜色  
+            foreground:"#000000"  //二维码颜色  
+      
+        });  
+    } 
+    init(); 
+    function init() {  
+        generateQRCode("canvas",205, 205, "http://www.baidu.com");  
+    }
+    function utf16to8(str) {  
+        var out, i, len, c;  
+        out = "";  
+        len = str.length;  
+        for (i = 0; i < len; i++) {  
+            c = str.charCodeAt(i);  
+            if ((c >= 0x0001) && (c <= 0x007F)) {  
+                out += str.charAt(i);  
+            } else if (c > 0x07FF) {  
+                out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));  
+                out += String.fromCharCode(0x80 | ((c >> 6) & 0x3F));  
+                out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));  
+            } else {  
+                out += String.fromCharCode(0xC0 | ((c >> 6) & 0x1F));  
+                out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));  
+            }  
+        }  
+        return out;  
+    }
+})
---
+++ php/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$_name = $_GET["name"]; 
+$_phone = $_GET["phone"];
+$_company = $_GET["company"];
+$_brand = $_GET["brand"];
+$_job = $_GET["job"];
+  $arr = array(
+      'qrcodeNum' => '201909182323'
+  );
+  $json_string = json_encode($arr);
+
+    echo $json_string;
+
+?>
